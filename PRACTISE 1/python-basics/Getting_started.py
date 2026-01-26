4d4af931325d9911067776621ce12fa6057827af
25B030853 <znrsafat@gmail.com>
2026-01-26 15:59:38 +0500

    Added two topics from W3school with examples

---
+++ PRACTISE 1/python-basics/Getting_started.py
@@ -0,0 +1,13 @@
+print("Hello world") #1 example prints hello world
+
+import sys
+
+print(sys.version) #to check python version 2 example
+
+exit() # 3 example, exits code
+
+name = "Rustam"
+print(f"Hello {name}") #4 example fixed string
+
+print(f"{input()} how are you?") #5 example 
+
---
+++ PRACTISE 1/python-basics/HOME.py
@@ -0,0 +1,9 @@
+print("Hello world!") #1 example
+
+print("Hi! I am student") #2 example
+
+print("Hello i am Nursafat") #3 example
+
+print("This is print function") #4 example
+
+print("How are you?") #5 example
