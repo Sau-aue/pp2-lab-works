2aba8fcd0fed54f49b069fc1c8b7b2e62c64ca77
25B030853 <znrsafat@gmail.com>
2026-01-27 21:45:39 +0500

    Python syntax explained with examples to clarify

---
+++ PRACTISE 1/python-basics/syntax.py
@@ -0,0 +1,27 @@
+# We can execute python syntax directly in command line >>> print("Hello, World!")
+# or by .py extention and running this file in command line C:\Users\Your Name>python myfile.py
+# 1 example python identation
+if 5 > 1:
+    print("5 is bigger") # identation is important to indicate block of the code
+#2 example
+"""
+if 5 > 1
+print("5 is bigger") incorrect 
+"""
+
+#3 example we choose number of spaces by ourself
+if 1 == 1:
+ print("equal")
+else:
+        print("not equal")
+
+#4 example what we should not do
+"""
+if a > b
+   print("a is bigger")
+      print("b is smaller") incorrect
+"""
+#5 example we can use ; between multiple statements
+
+print("Hello world!") ; print("How are you?") ; print("I am fine thank you")
+
