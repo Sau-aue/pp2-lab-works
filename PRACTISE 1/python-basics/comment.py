1c83b8d031f578a209bd52e228265f499657fd9d
25B030853 <znrsafat@gmail.com>
2026-01-27 21:54:55 +0500

    Python comment versions with examples

---
+++ PRACTISE 1/python-basics/comment.py
@@ -0,0 +1,25 @@
+# 1 example this is comment
+print("Hello world") #prints hello world
+
+#2 example multiline comments
+#this is
+#multiline
+#comment
+print("multiline comment")
+
+#3 example multiline string
+"""
+This is multiline string
+print(a+b)
+print(a-b)
+"""
+
+#4 example 
+#print("Hello Martin!")
+age = int(input("Your age?"))
+
+#5 example 
+"""
+This code prints greetings :)
+"""
+print("Hi Mate!")
