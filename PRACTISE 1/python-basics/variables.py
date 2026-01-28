8a5a4fa599ef3902dc748fb5de46b0e2c036f389
25B030853 <znrsafat@gmail.com>
2026-01-28 21:00:02 +0500

    Added variables.py with examples for variables

---
+++ PRACTISE 1/python-basics/variables.py
@@ -0,0 +1,23 @@
+# 1 example how to know variables type?
+x = 4
+y = "Jake"
+print(type(x)) ; print(type(y))
+
+#2 example "" or '' ?
+#both of them are acceptable
+name = "Kate"
+surname = 'Katherine'
+
+#3 example case sensative
+Z = "Oreo"
+z = "oreo" #small z will not overwrite upper Z
+
+#4 example assign multiple variables
+a,b,c = "USA","Russia","Spain"
+print(a); print(b) ; print(c)
+
+#5 example One value to multiple variables
+r=t=i="KBTU"
+print(r)
+print(t)
+print(i)
