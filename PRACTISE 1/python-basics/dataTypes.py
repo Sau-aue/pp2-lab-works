9a6fe2a1a245a0f4eb12fe4e628a293246612271
25B030853 <znrsafat@gmail.com>
2026-01-28 21:07:59 +0500

    Added dataTypes.py with examples for data types and their division

---
+++ PRACTISE 1/python-basics/dataTypes.py
@@ -0,0 +1,21 @@
+# 1 example boolean
+is_digit = True
+is_even = False
+
+#2 example text(string)
+name = "Anderson"
+surname = "Jack"
+
+# 3 example None type
+emptyness = None
+print(type(emptyness)) # prints None for type
+
+# 4 example sequence 
+fruits = ["banana","apple","orange"] # list
+subject = ("math" , "PP2") # tuple
+set_of_data = {"int","str", "boolean"} #set
+
+# 5 example numbers\digits
+age = 29 #int
+balance = 2556.98 #float
+
