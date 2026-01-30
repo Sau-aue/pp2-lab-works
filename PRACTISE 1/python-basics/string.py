d6c55d67014f94bc1fbbabe004e823ee5240a9a5
25B030853 <znrsafat@gmail.com>
2026-01-30 18:39:47 +0500

    Practise 1 string topic added with examples

---
+++ PRACTISE 1/python-basics/string.py
@@ -0,0 +1,23 @@
+# 1 example quotes inside quote
+print("Hey !! How are you? How is 'Anderson'")
+
+# 2 example multiline string
+a = """I will go to the university so do not call me ok!
+Because i will not be able to answer you."""
+print(a)
+
+# 3 example a[start:end:step] to slice or substract words from string
+b = "C++ is fun"
+print(a[::-1]) # prints in reversed order
+
+# 4 example modify string
+p = "Hi i am your friend"
+print(p.upper()) # prints in upper case
+print(p.lower()) # prints in lower case
+
+# 5 example strips function and replace
+name = " Nurzhan Alibek "
+print(name.strip()) # removes any whitespace form the beginning and the end
+name2 = "Nurzhan"
+name_replaced = name2.replace("N" , "z") #replaces string to the string
+print(name_replaced)
