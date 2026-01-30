3d44aee83585a679ab7db637d300097c120e6cff
25B030853 <znrsafat@gmail.com>
2026-01-30 18:30:02 +0500

    Practise 1 casting topic added with examples

---
+++ PRACTISE 1/python-basics/casting.py
@@ -0,0 +1,23 @@
+#1 example int()
+
+b = int(1236.45648)
+c = int(1236)
+
+# 2 example str()
+s = str("s1")
+d = str(3.784)
+f = str(12364)
+
+# 3 example float()
+p = float(3)
+
+
+# 4 example
+g = float(45.9)
+op = float("1236.6")
+
+# 5 example 
+a = int("1568")
+
+
+
