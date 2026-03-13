1af89bde17fae778d99469ac4b7a45b376a6131b
25B030853 <znrsafat@gmail.com>
2026-03-14 04:43:00 +0500

    Added Demonstrate type checking and conversions

---
+++ Practise6/Demonstrate type checking and conversions.py
@@ -0,0 +1,23 @@
+# Type checking and conversion examples
+
+# 1) type()
+x = 10
+print(type(x))
+
+# 2) int()
+a = "25"
+print(int(a) + 5)
+
+# 3) float()
+b = "3.14"
+print(float(b))
+
+# 4) str()
+c = 100
+print(str(c) + " dollars")
+
+# 5) bool()
+print(bool(0))    # False
+print(bool(5))    # True
+print(bool(""))   # False
+print(bool("hi")) # True
